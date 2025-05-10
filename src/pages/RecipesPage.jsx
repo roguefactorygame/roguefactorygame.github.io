59542899a95cc469045b4b1d1af3b11debfea690
roguefactorygame <roguefactorygame@protonmail.com>
2025-05-10 15:43:33 +0000

    some styles

---
+++ src/pages/RecipesPage.jsx
@@ -0,0 +1,11 @@
+import RecipeGraph from "../components/RecipeGraph";
+import PageContainer from "../components/PageContainer";
+
+export default function RecipesPage() {
+  return (
+    <PageContainer>
+      <h1>Recipes</h1>
+      <RecipeGraph />
+    </PageContainer>
+  );
+}
