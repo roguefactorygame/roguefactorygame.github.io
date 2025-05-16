31e26af4038adc687ae70443e0e99cf3ec12a94c
Jarrod Robinson <jtrobinson1993@gmail.com>
2025-05-15 19:26:01 -0500

    move navigation outside pagecontent

--- src/components/layout/PageContent.jsx
+++ src/components/layout/PageContent.jsx
@@ -3,9 +3,11 @@ import styles from "./PageContent.module.css";
 
 export const PageContent = ({ children }) => {
   return (
-    <div className={styles.PageContent}>
+    <>
       <Navigation />
-      <div style={{ position: "relative", flexGrow: 1 }}>{children}</div>
-    </div>
+      <div className={styles.PageContent}>
+        <div style={{ position: "relative", flexGrow: 1 }}>{children}</div>
+      </div>
+    </>
   );
 };
