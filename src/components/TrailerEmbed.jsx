7c91c6726ad1fa03de26c242831a47907236300a
roguefactorygame <roguefactorygame@protonmail.com>
2026-01-12 03:24:12 +0000

    update trailer

--- src/components/TrailerEmbed.jsx
+++ src/components/TrailerEmbed.jsx
@@ -1,14 +1,24 @@
+import styles from "./TrailerEmbed.module.css";
+
 export const TrailerEmbed = () => {
   return (
-    <iframe
-      width="600"
-      height="338"
-      src="https://www.youtube.com/embed/7GmhGbojdMY?si=1JgzoJERErCn4Xla"
-      title="YouTube video player"
-      frameborder="0"
-      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-      referrerpolicy="strict-origin-when-cross-origin"
-      allowfullscreen
-    />
+    <div className={styles.TrailerEmbed}>
+      <div className={styles.TrailerEmbedContent}>
+        <iframe
+          // width="560"
+          // height="315"
+          src="https://www.youtube.com/embed/kFkxq68oBa4?si=PCwppq8ePJWv6e2p"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerpolicy="strict-origin-when-cross-origin"
+          allowfullscreen
+          style={{
+            width: "50vw",
+            aspectRatio: "560/315",
+          }}
+        />
+      </div>
+    </div>
   );
 };
